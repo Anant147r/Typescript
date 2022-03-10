5992b7489de7e9732c8523ffe9ad5ff3ec3abbc8
Anant Rawat <anant.rawat@sunfox.in>
2022-03-10 13:54:21 +0530

    Types added

--- main.js
+++ main.js
@@ -13,3 +13,32 @@ name = undefined;
 name = null;
 statement = undefined;
 statement = null;
+// ARRAY
+var arr = [1, 2, 3, 4];
+var arr2 = [1, 2, 3, 4, 5];
+// Tupple
+var tupple = ["Anant", 1];
+// ENUM
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Yellow"] = 2] = "Yellow";
+})(Color || (Color = {}));
+var c = Color.Yellow;
+console.log(c);
+// Any
+var value = "Anant";
+value = 2;
+value = true;
+console.log(value);
+// Type Inference -- works during initialization
+var newValue;
+newValue = 10;
+newValue = true;
+var newValue2 = 4;
+// newValue2=true      It will show error because of type inference
+// MULTITYPE
+var multitype;
+multitype = true;
+multitype = 6;
--- main.ts
+++ main.ts
@@ -16,3 +16,40 @@ name = null;
 
 statement = undefined;
 statement = null;
+
+// ARRAY
+let arr: number[] = [1, 2, 3, 4];
+let arr2: Array<number> = [1, 2, 3, 4, 5];
+
+// Tupple
+let tupple: [string, number] = ["Anant", 1];
+
+// ENUM
+enum Color {
+  Red,
+  Green,
+  Yellow,
+}
+let c: Color = Color.Yellow;
+console.log(c);
+
+// Any
+let value: any = "Anant";
+value = 2;
+value = true;
+console.log(value);
+
+// Type Inference -- works during initialization
+
+let newValue;
+newValue = 10;
+newValue = true;
+
+let newValue2 = 4;
+// newValue2=true      It will show error because of type inference
+
+// MULTITYPE
+
+let multitype: number | boolean;
+multitype = true;
+multitype = 6;
