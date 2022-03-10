30c2ff7cc93030500fb0ff4f775423ded8ad7dc9
Anant Rawat <anant.rawat@sunfox.in>
2022-03-10 10:45:32 +0530

    Variable Declaration added

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var message = "Hello World";
+var name = "Anant";
+console.log(message);
+console.log(name);
+
+const value = 8;
---
+++ main.ts
@@ -0,0 +1,5 @@
+export {};
+let message = "Hello World";
+let name = "Anant";
+console.log(message);
+console.log(name);
